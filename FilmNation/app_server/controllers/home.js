1f54f84acf36846f2dfe339deb897bd3cbefbc57
Jared V. Mc Inerney <Jared.V.McInerney@students.ittralee.ie>
2018-10-24 16:55:42 +0100

    additional pug file film added
    EX11

--- FilmNation/app_server/controllers/home.js
+++ FilmNation/app_server/controllers/home.js
@@ -8,7 +8,7 @@ res.render('index', { title: 'Home info' });
 };
 /* GET 'Add review' page */
 const filmInfo = function(req, res){
-res.render('index', { title: 'Film Name' });
+res.render('film', { title: 'Film Name' });
 };
 module.exports = {
 filmList,
